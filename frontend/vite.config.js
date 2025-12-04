d9436ddae03c36256e5105743b1eab14ac3d35fc
Piya Saenchu <piyawork52@gmail.com>
2025-12-05 03:14:49 +0700

    feat: enhance Vite configuration with detailed server proxy settings and build options

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
+
     proxy: {
-      "/api": "http://localhost:3000"
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      }
     }
   },
+
+  build: {
+    outDir: "dist", // โฟลเดอร์ปลายทาง
+    emptyOutDir: true, // ล้างไฟล์เก่าก่อน build
+    sourcemap: false, // ไม่สร้าง Source map ใน Production (เพื่อความปลอดภัยและลดขนาด)
+    chunkSizeWarningLimit: 1000, // ขยายลิมิตการแจ้งเตือนขนาดไฟล์ (optional)
+    rollupOptions: {
+      output: {
+        // แยกไฟล์ Vendor (Library) ออกจากไฟล์ Code หลัก เพื่อการ Cache ที่ดีขึ้น
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
