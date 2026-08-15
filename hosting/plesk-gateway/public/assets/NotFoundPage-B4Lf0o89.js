17e943981d394174c1253e756207ccf3dbf1e394
Piya Saenchu <piyawork52@gmail.com>
2026-08-15 11:26:20 +0700

    Publish responsive Plesk frontend

---
+++ hosting/plesk-gateway/public/assets/NotFoundPage-B4Lf0o89.js
@@ -0,0 +1,1 @@
+import{j as o}from"./mui-C_Qb5ZDJ.js";function n(){return o.jsx("div",{children:"404 Not Found"})}export{n as default};
--- hosting/plesk-gateway/public/assets/NotFoundPage-D_hZVAbF.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./mui-CVMMoN_Z.js";function n(){return o.jsx("div",{children:"404 Not Found"})}export{n as default};
